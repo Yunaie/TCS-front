edf68da891eaf4dde08d8d4504f73fc12ed51694
Yunaie <119615317+Yunaie@users.noreply.github.com>
2023-06-04 20:49:46 +0200

    Update PosterCriminelPage.js

--- src/components/page/PosterCriminelPage.js
+++ src/components/page/PosterCriminelPage.js
@@ -1,7 +1,7 @@
 
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../../styles/ArticlePosterPage.css";
+import "../../styles/ArticleposterPage.css";
 import axios from "axios";
 
 function PosterVictimePage() {
