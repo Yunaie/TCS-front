c9d5cbc91c3fdba4d4a07ddc85bec0e7720295bd
Yunaie <119615317+Yunaie@users.noreply.github.com>
2023-06-04 20:57:28 +0200

    Update PosterArticlePage.js

--- src/components/page/PosterArticlePage.js
+++ src/components/page/PosterArticlePage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../../styles/ArticleposterPage.css";
+import "../../styles/LoginPage.css";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
